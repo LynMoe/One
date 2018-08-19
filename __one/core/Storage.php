338204d0b84628a78dc6ffba6f26cba51ca462df
XiaoLin <i@cgl.li>
2018-08-19 22:17:08 +0800

    添加curl参数

--- __one/core/Storage.php
+++ __one/core/Storage.php
@@ -15,6 +15,7 @@ class Storage
         $ch = curl_init();
 
         curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
