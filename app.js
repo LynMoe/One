39c8382b5406f0d754356abcd76143cab088a4e5
XiaoLin <lolilin@outlook.com>
2019-09-13 00:49:02 +0800

    Beautify the code

--- app.js
+++ app.js
@@ -1,116 +1,119 @@
-const fs = require('fs');
-const url = require('url');
-const request = require('request');
-const fileType = require('file-type');
+const fs = require("fs");
+const url = require("url");
+const request = require("request");
+const fileType = require("file-type");
 const rimraf = require("rimraf");
 const http = require("http");
-const sharp = require('sharp');
-const cleanCss = require('clean-css');
-const zlib = require('zlib');
+const sharp = require("sharp");
+const cleanCss = require("clean-css");
+const zlib = require("zlib");
 
-String.prototype.hashCode = function () {
+const hashCode = function () {
     var hash = 0,
         i, chr;
     if (this.length === 0) return hash;
     for (i = 0; i < this.length; i++) {
         chr = this.charCodeAt(i);
         hash = ((hash << 5) - hash) + chr;
-        hash |= 0; // Convert to 32bit integer
+        hash |= 0;
     }
     return hash;
 };
 
-http.createServer(handler).listen(3000);
-
 async function handler(req, res) {
     process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
-    let DIR = __dirname + '/';
+    let DIR = __dirname + "/";
     let filedata, filehash, fileinfo, filemetadata, fileurl, pathname, namespace, settings;
 
-    // console.log('JavaScript HTTP trigger function processed a request.');
+    // console.log("JavaScript HTTP trigger function processed a request.");
 
-    if (!fs.existsSync(DIR + 'Cache'))
-        fs.mkdirSync(DIR + 'Cache');
-    DIR = DIR + 'Cache/';
+    if (!fs.existsSync(DIR + "Cache"))
+        fs.mkdirSync(DIR + "Cache");
+    DIR = DIR + "Cache/";
 
     if (pathname = req.url) {
         if (url.parse(pathname).search)
             pathname = url.parse(pathname).pathname + url.parse(pathname).search;
         else
             pathname = url.parse(pathname).pathname;
-        // console.log('Pathname: ' + pathname);
+        // console.log("Pathname: " + pathname);
 
-        namespace = pathname.split('/', 2);
+        namespace = pathname.split("/", 2);
         if (namespace)
-            namespace = namespace[1].replace(/ /g, '');
-        console.log(`Namespace: ${namespace}`);
+            namespace = namespace[1].replace(/ /g, "");
+        // console.log(`Namespace: ${namespace}`);
         pathname = pathname.substr(namespace.length + 2);
 
-        if (fs.existsSync(DIR = DIR + namespace + '/') && namespace && fs.existsSync(DIR + 'settings.json')) {
-            // console.log('Namespace exist.');
-            settings = JSON.parse(fs.readFileSync(DIR + 'settings.json'));
+        if (fs.existsSync(DIR = DIR + namespace + "/") && namespace && fs.existsSync(DIR + "settings.json")) {
+            // console.log("Namespace exist.");
+            settings = JSON.parse(fs.readFileSync(DIR + "settings.json"));
             // console.log(settings);
-            // console.log('File name: ' + pathname);
+            // console.log("File name: " + pathname);
 
-            filehash = pathname.hashCode();
-            // console.log('File path hash: ' + filehash);
+            filehash = hashCode(pathname);
+            // console.log("File path hash: " + filehash);
 
-            if (!fs.existsSync(DIR = DIR + 'data/'))
+            if (!fs.existsSync(DIR = DIR + "data/"))
                 fs.mkdirSync(DIR);
 
-            DIR = DIR + filehash + '/';
+            DIR = DIR + filehash + "/";
 
             fileurl = settings.host + pathname;
-            // console.log('File url: ' + fileurl);
+            // console.log("File url: " + fileurl);
 
-            if (fs.existsSync(DIR) && fs.existsSync(DIR + 'info.json') &&
-                fs.statSync(DIR + 'file.data').mtime.getTime() > (Date.now() - settings.expireTime) &&
-                fs.existsSync(DIR + 'file.br.data')) {
-                fileinfo = JSON.parse(fs.readFileSync(DIR + 'info.json'));
+            if (fs.existsSync(DIR) && fs.existsSync(DIR + "info.json") &&
+                fs.statSync(DIR + "file.data").mtime.getTime() > (Date.now() - settings.expireTime) &&
+                fs.existsSync(DIR + "file.br.data")) {
+                fileinfo = JSON.parse(fs.readFileSync(DIR + "info.json"));
 
                 sent = true;
-                // console.log('File type:' + fileinfo.type);
+                // console.log("File type:" + fileinfo.type);
 
-                res.setHeader('Content-Type', fileinfo.type);
-                res.setHeader('Cache-Control', `max-age=${settings.expireTime / 1000}`);
-                res.setHeader('Access-Control-Allow-Origin', '*');
+                res.setHeader("Content-Type", fileinfo.type);
+                res.setHeader("Cache-Control", `max-age=${settings.expireTime / 1000}`);
+                res.setHeader("Access-Control-Allow-Origin", "*");
 
-                res.setHeader('Vary', 'Accept-Encoding');
-                res.setHeader('Server','Node');
-                res.setHeader('X-Powered-By','One');
+                res.setHeader("Vary", "Accept-Encoding");
+                res.setHeader("X-Powered-By", "One");
 
-                let acceptEncoding = req.headers['accept-encoding'];
+                let acceptEncoding = req.headers["accept-encoding"];
                 if (!acceptEncoding) {
-                    acceptEncoding = '';
+                    acceptEncoding = "";
                 }
 
                 if (/\bbr\b/.test(acceptEncoding)) {
-                    res.setHeader('Content-Encoding','br');
-                    res.end(fs.readFileSync(DIR + 'file.br.data', {encoding: null}));
+                    res.setHeader("Content-Encoding", "br");
+                    res.end(fs.readFileSync(DIR + "file.br.data", {
+                        encoding: null
+                    }));
                 } else if (/\bgzip\b/.test(acceptEncoding)) {
-                    res.setHeader('Content-Encoding','gzip');
-                    res.end(fs.readFileSync(DIR + 'file.gz.data', {encoding: null}));
+                    res.setHeader("Content-Encoding", "gzip");
+                    res.end(fs.readFileSync(DIR + "file.gz.data", {
+                        encoding: null
+                    }));
                 } else {
-                    res.setHeader('Content-Encoding','deflate');
-                    res.end(fs.readFileSync(DIR + 'file.data', {encoding: null}));
+                    res.setHeader("Content-Encoding", "deflate");
+                    res.end(fs.readFileSync(DIR + "file.data", {
+                        encoding: null
+                    }));
                 }
 
                 return;
             } else {
                 res.writeHead(307, {
-                    'Location': fileurl,
+                    "Location": fileurl,
                 });
                 sent = true;
-                res.end('Moved');
+                res.end("Moved");
 
-                if (fs.existsSync(DIR + 'do.lock') && (fs.statSync(DIR + 'do.lock').mtime.getTime() + 60000) > Date.now()) {
+                if (fs.existsSync(DIR + "do.lock") && (fs.statSync(DIR + "do.lock").mtime.getTime() + 60000) > Date.now()) {
                     return;
                 }
 
                 if (!fs.existsSync(DIR))
                     fs.mkdirSync(DIR);
 
-                fs.writeFileSync(DIR + 'do.lock', Date.now(), 'utf8');
+                fs.writeFileSync(DIR + "do.lock", Date.now(), "utf8");
 
                 sent = true;
                 return new Promise((resolve, reject) => {
@@ -118,8 +121,8 @@ async function handler(req, res) {
                         url: fileurl,
                         encoding: null,
                         headers: {
-                            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36',
-                            'cache-control': 'max-age=0',
+                            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36",
+                            "cache-control": "max-age=0",
                         },
                         timeout: 10000
                     }, async (error, response, body) => {
@@ -134,17 +137,17 @@ async function handler(req, res) {
                         }
 
                         if (!(fileType(body) && (filemetadata = fileType(body).mime)))
-                            filemetadata = response.headers['content-type'];
+                            filemetadata = response.headers["content-type"];
 
                         // console.log(filemetadata);
 
-                        if (['image/png', 'image/jpg', 'image/jpeg'].includes(filemetadata)) {
+                        if (["image/png", "image/jpg", "image/jpeg"].includes(filemetadata)) {
                             try {
                                 body = await sharp(body)
                                     .webp()
                                     .toBuffer();
 
-                                filemetadata = 'image/webp';
+                                filemetadata = "image/webp";
                             } catch (e) {
                                 // console.log(e);
                                 rimraf.sync(DIR);
@@ -153,28 +156,28 @@ async function handler(req, res) {
                             }
                         }
 
-                        if (filemetadata.indexOf('text/css') !== -1) {
+                        if (filemetadata.indexOf("text/css") !== -1) {
                             let data = new cleanCss({}).minify(body.toString());
                             if (data.errors.length !== 0) {
                                 // console.log(data.errors);
-                            } else 
+                            } else
                                 body = Buffer.from(data.styles);
                         }
 
                         filemetadata = {
-                            'type': filemetadata,
-                            'size': body.length,
-                            'time': Date.now(),
+                            "type": filemetadata,
+                            "size": body.length,
+                            "time": Date.now(),
                         };
                         // console.log(`[${response.statusCode}] ${filemetadata}`);
 
-                        fs.writeFileSync(DIR + 'file.data', body, 'binary');
-                        fs.writeFileSync(DIR + 'file.gz.data', zlib.gzipSync(body, {
+                        fs.writeFileSync(DIR + "file.data", body, "binary");
+                        fs.writeFileSync(DIR + "file.gz.data", zlib.gzipSync(body, {
                             level: 9
-                        }), 'binary');
-                        fs.writeFileSync(DIR + 'file.br.data', zlib.brotliCompressSync(body), 'binary');
-                        fs.writeFileSync(DIR + 'info.json', JSON.stringify(filemetadata), 'utf8');
-                        fs.unlinkSync(DIR + 'do.lock');
+                        }), "binary");
+                        fs.writeFileSync(DIR + "file.br.data", zlib.brotliCompressSync(body), "binary");
+                        fs.writeFileSync(DIR + "info.json", JSON.stringify(filemetadata), "utf8");
+                        fs.unlinkSync(DIR + "do.lock");
 
                         resolve();
                     });
@@ -186,3 +189,5 @@ async function handler(req, res) {
         }
     }
 }
+
+http.createServer(handler).listen(3000);
